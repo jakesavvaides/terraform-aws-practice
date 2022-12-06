c27033715f317303927d14366f43f7bcb1327118
Jake Savvaides <jake.savvaides@mavenwave.com>
2022-12-06 12:39:32 -0600

    Changes to terraform file for creating an aws ec2 instance

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region  = "us-east-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-05803413c51f242b7"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "JakesExampleAppServerInstance"
   }
 }
